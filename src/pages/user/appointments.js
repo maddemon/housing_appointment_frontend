f471d9a3d1bb5e55aa27a5ce7774f085cf12386d
Liangjun Zheng <zhengliangjun@outlook.com>
2019-04-12 10:29:31 +0800

    添加我的预约和密码修改页面

--- src/pages/user/appointments.js
+++ src/pages/user/appointments.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import { PageHeader, Card } from 'antd'
 
 @inject('stores')
 @observer
-export default class UserReservePage extends Component {
+export default class UserAppointmentsPage extends Component {
     componentWillMount() {
         this.props.stores.globalStore.setTitle('我的预约');
     }
     render() {
+        const list = this.props.stores.appointmentStore.myList;
         return (
             <div>
-                我的预约
+                <PageHeader title="我的预约" subTitle="查询您的预约记录" />
+                {list.map(item => <UserAppointmentItemControl model={item} />)}
             </div>
         )
     }
+}
+
+const UserAppointmentItemControl = (props) => {
+    const model = props.model;
+    return <Card title={model.batchName}>
+        <p>预约时间：{new Date(model.createTime).toLocaleString()}</p>
+        <p>所用指标：{model.quotaUuid}</p>
+        <p>房屋地址：{model.batchAddress}</p>
+        <p>选房时间：{model.chooseTime}</p>
+        <p>选房地点：{model.chooseAddress}</p>
+    </Card>
 }
--- src/pages/user/edit_password.js
+++ src/pages/user/edit_password.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
-import { Button } from 'antd'
+import { PageHeader, Form, Icon, Input, Button, Row, Col, message } from 'antd'
+
 @inject('stores')
 @observer
-export default class EditPasswordPage extends Component {
+class EditPasswordPage extends Component {
+
+    state = { loading: false }
+
     componentWillMount() {
-        console.log('EditPasswordPage')
         this.props.stores.globalStore.setTitle('修改密码');
     }
 
+    handleSubmit = (e) => {
+        this.setState({ loading: true });
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                if (values.newpassword !== values.repassword) {
+                    message.warn("两次输入密码不相同");
+                    this.setState({ loading: false })
+                    return false;
+                }
+                this.props.stores.userStore.editPassword(values.oldpassword, values.newpassword)
+                this.props.history.push('/')
+            } else {
+                this.setState({ loading: false });
+            }
+        });
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                <Button>修改密码</Button>
-            </div>
+            <Row>
+                <PageHeader title="修改密码" />
+                <Col xxl={{ span: 8, offset: 8 }} xl={{ span: 8, offset: 8 }} lg={{ span: 12, offset: 12 }}>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Item label="旧密码">
+                            {getFieldDecorator('oldpassword', { rules: [{ required: true, message: '请输入旧密码', }], })(
+                                <Input type="password" size="large" placeholder="请输入旧密码" />
+                            )}
+                        </Form.Item>
+                        <Form.Item label="新密码">
+                            {getFieldDecorator('newpassword', { rules: [{ required: true, message: '请输入新的密码', }], })(
+                                <Input type="password" size="large" placeholder="请输入新的密码" />
+                            )}
+                        </Form.Item>
+                        <Form.Item label="确认密码">
+                            {getFieldDecorator('repassword', { rules: [{ required: true, message: '请再输入一遍新密码', }], })(
+                                <Input type="password" size="large" placeholder="请再输入一遍新密码" />
+                            )}
+                        </Form.Item>
+                        <Form.Item className="additional">
+                            <Button size="large" loading={this.state.loading} type="primary" htmlType="submit">
+                                <Icon type="check" />提交
+                            </Button>
+                        </Form.Item>
+
+                    </Form>
+                </Col>
+            </Row>
         )
     }
 }
+const editPasswordPage = Form.create()(EditPasswordPage);
+export default editPasswordPage;
